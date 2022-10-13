84a7fd7a1e10847cef4ff967aa325969806886e8
nbaipureddy <nbaipureddy@edubrite.com>
2022-10-13 19:05:15 +0530

    UI-Changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "91408428e90a86e9"; }
+/******/ 		__webpack_require__.h = function() { return "92d3c07e5fbf4437"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/Classify.js
+++ pages/Classify.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import IMAGES from '../Images/Images'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import {ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/solid";
+import {ChevronRightIcon, ChevronLeftIcon ,PaperAirplaneIcon } from "@heroicons/react/solid";
 import { motion } from "framer-motion";
 
 const Classify = () => {
@@ -27,16 +27,16 @@ const Classify = () => {
   };
   return (
     <div className="flex  bg-gray-100 min-h-screen lg:w-full lg:justify-center">
-      <div className="mx-auto w-[3rem] h-[54rem] mt-10 flex justify-center max-w-4xl">
+      <div className="mx-auto w-full h-[54rem] mt-10 flex justify-center max-w-4xl">
         <Carousel
-         className="flex -ml-[40rem]"
+         className="flex"
          statusFormatter={(currentItem,total) => (
-           <div className="absolute font-sans group top-[38rem] right-[18.5rem] w-[5rem] text-[1rem] text-darkBg">{currentItem} / {total}</div>
+           <div className="absolute font-sans group top-[36rem] right-[23.5rem] w-[5rem] text-[1rem] text-darkBg">{currentItem} / {total}</div>
          )}
         renderArrowPrev={(clickHandler, hasPrev, labelPrev) => (
           <motion.div
-          className="absolute group top-[37rem] left-[10rem] cursor-pointer"
-          onClick={clickHandler}
+          className="absolute group top-[37rem] left-[10rem] cursor-pointer hidden"
+          //onClick={clickHandler}
           whileTap={{ scale: 0.95 }} 
             
           >
@@ -44,35 +44,48 @@ const Classify = () => {
           </motion.div>
         )}
         renderArrowNext={(clickHandler, hasNext, labelNext) => (
+
+
+
           <motion.div
-          className="absolute group top-[37rem] left-[13rem] ml-60"
+          className="absolute group top-[39rem] left-[9rem] ml-60"
           whileTap={{ scale: 0.95 }}  
            
           >
-            <ChevronRightIcon  onClick={clickHandler} className="w-14 h-14 cursor-pointer rounded-full bg-darkBgLight p-4 text-gray-400 transition duration-500 ease-in-out  group-hover:bg-gray-500 group-hover:text-darkBgLight" />
+            <motion.div
+            onClick={clickHandler}
+            className="group cursor-pointer rounded-[0.2rem] bg-gray-800 font-ubuntu text-lg font-semibold text-white shadow-md shadow-gray-800 transition duration-500 ease-in-out  lg:hover:bg-blue-400 lg:hover:text-darkBgLight"
+          >
+           <div className="flex h-16 w-44 space-x-4 justify-center items-center transition-transform duration-500 ease-in-out group-hover:scale-105">
+            <p className="tracking-[0.2rem] text-[1.5rem]">Next</p>
+            <PaperAirplaneIcon className="h-7 w-7 animate-pulse rotate-90 hover:text-darkBgLight" />
+          </div>
+          </motion.div>
           </motion.div>
        )}
         
-        renderIndicator={(onClickHandler, isSelected, index, label) => {
-        return (
-          <span
-           // style={style}
-            className={isSelected ? 'bg-gray-500 transition duration-500 ease-in-out  hover:text-darkBgLight text-darkBgLight hover:bg-gray-500 rounded-full p-3 mr-2 mb-2 w-12 h-12 flex-wrap cursor-pointer' : 'bg-darkBgLight mb-2 transition duration-500 ease-in-out  hover:bg-gray-500 rounded-full p-3 mr-2 text-gray-300 w-12 h-12 flex-wrap cursor-pointer'}
-            onClick={onClickHandler}
-            onKeyDown={onClickHandler}
-            value={index}
-            key={index}
-            role="button"
-            tabIndex={0}
-            aria-label={`${label} ${index + 1}`}
-          >
-            {index+1}
-          </span>
-        );
-      }} showThumbs={false}>  
+      //   renderIndicator={(onClickHandler, isSelected, index, label) => {
+      //   return (
+      //     <span
+      //      // style={style}
+      //       className={isSelected ? 'bg-gray-500 transition duration-500 ease-in-out  hover:text-darkBgLight text-darkBgLight hover:bg-gray-500 rounded-full p-3 mr-2 mb-2 w-12 h-12 flex-wrap cursor-pointer' : 'bg-darkBgLight mb-2 transition duration-500 ease-in-out  hover:bg-gray-500 rounded-full p-3 mr-2 text-gray-300 w-12 h-12 flex-wrap cursor-pointer'}
+      //       onClick={onClickHandler}
+      //       onKeyDown={onClickHandler}
+      //       value={index}
+      //       key={index}
+      //       role="button"
+      //       tabIndex={0}
+      //       aria-label={`${label} ${index + 1}`}
+      //     >
+      //       {index+1}
+      //     </span>
+      //   );
+      // }}
+       showIndicators={false}
+       showThumbs={false}>  
           {IMAGES.map((image)=>(
             <div>   
-                <img className="h-[34rem] object-contain border-r-2" id={image.id} src={image.path}></img>
+                <img className="h-[34rem] object-contain border-r-4" id={image.id} src={image.path}></img>
             </div>
           ))}
         </Carousel>
